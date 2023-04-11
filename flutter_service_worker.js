626cb050be2dd77dadf0c8fac126f229c5409ffc
Eduardo Lopez <zorak.dev@gmail.com>
2023-04-10 22:52:34 -0300

    change fontsize for movie title

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "02d12ac2544865418194b88b3fc6ef7a",
-"/": "02d12ac2544865418194b88b3fc6ef7a",
+  "index.html": "a726452f0042e1b9759c4ce3ff135eb3",
+"/": "a726452f0042e1b9759c4ce3ff135eb3",
 "version.json": "d391f58c341c6f1b242300f83302cc7c",
-"main.dart.js": "b390c5fa4ecf7f11e0736e3c36cb5fdb",
+"main.dart.js": "d9a99859d1d61d8154cdf5c620af10ee",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
