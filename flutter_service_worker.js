e7a9d44c3439fabb3d96fe88349a7129f1c47f58
Zorak <47281786+zorakDev@users.noreply.github.com>
2023-08-30 02:56:53 -0300

    semitransparent title background

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "53df48903d06fc74392358a7cb647b73",
-"/": "53df48903d06fc74392358a7cb647b73",
+const RESOURCES = {"index.html": "78258909fa382970352ad4da97d71c3b",
+"/": "78258909fa382970352ad4da97d71c3b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "c52367d0afac3987757c13fb00d9d26a",
+"main.dart.js": "f00471810bbc80e14895bdfe6eca2dcf",
 "version.json": "d391f58c341c6f1b242300f83302cc7c",
 "canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
 "canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
